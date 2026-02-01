5d08b5a62e6a6bb1819afbd586d11cb14ade0ff7
adoleiiiiii <787314057@qq.com>
2026-02-02 01:23:24 +0800

    fix(urls): 修复DEBUG模式下媒体文件服务路由问题
    使用re_path替代static来正确处理媒体文件路由，确保在DEBUG模式下能够正常访问媒体文件

--- backend/application/urls.py
+++ backend/application/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +12,10 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    from django.views.static import serve
+
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
